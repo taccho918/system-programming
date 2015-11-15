3ad21964c8398740611d040878ce81e2cfffa5de
MomokaTakahashi <highbridge918@gmail.com>
2015-11-15 23:22:34 +0900

    rename file

--- 1-1-1.s
+++ 1-1-1.s
@@ -5,39 +5,28 @@ main:
 	move	$s0, $ra		# mainを呼んだ戻り先のアドレスが入っている
 					# $raを$s0に保存しておく
 	
-	li	$a0, 72			# 'H' = 72, $a0 = H 
-	jal	putc			# jamp and link to putc
-
+	li	$a0, 72					# 'H' = 72, $a0 = H 
+	jal	putc					# jamp and link to putc
 	li	$a0, 101		# 'a' = 101, $a0 = e
 	jal	putc			# 
-
 	li	$a0, 108		# 'l' = 108, $a0 = l
 	jal	putc			#
-	
 	li	$a0, 108		# 'l' = 108, $a0 = l
-	jal	putc			# 
-
+	jal	putc			#
 	li	$a0, 111		# 'o' = 111, $a0 = o
 	jal	putc			# 
-
 	la	$a0, 32
 	jal	putc			# print " "
-
 	li	$a0, 87			# 'W' = 87, $a0 = W
 	jal	putc			#
-
 	li	$a0, 111		# 'o' = 111, $a0 = o
 	jal	putc			#
-
 	li	$a0, 114		# 'r' = 114, $a0 = r
 	jal	putc			#
-
 	li	$a0, 108		# 'l' = 108, $a0 = l
 	jal	putc			#
-
 	li	$a0, 100		# 'd' = 100, $a0 = d
 	jal	putc			#
-
 
 	move	$ra, $s0		# $s0に保存しておいた戻り先を$raに入れる
 	j	$ra			# mainを呼んだ戻り先に飛ぶ
